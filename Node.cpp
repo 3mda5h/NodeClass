ca2751f49be617a315fc3ebbdac1c5171beac0d0
3mda5h <90864557+3mda5h@users.noreply.github.com>
2022-01-10 13:16:05 -0800

    Add files via upload

---
+++ Node.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include "Node.h"
+
+using namespace std;
+
+Node::Node(Student* newStudent)
+{
+  student = newStudent;
+  next = NULL;
+}
+
+Node::~Node()
+{
+  delete student;
+}
+
+void Node::setNext(Node* newNext)
+{
+  next = newNext;
+}
+
+Student* Node::getStudent()
+{
+  return student;
+}
+
+Node* Node::getNext()
+{
+  return next;
+}
+
